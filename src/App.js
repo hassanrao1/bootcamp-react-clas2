e51af68bacf250e59189edcfaef51e23b303bf7e
ItsHsn <47700761+HsnRao0335@users.noreply.github.com>
2020-06-11 03:33:24 +0500

    Bootcamp react class 2 playing with props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Learning from "./learning";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Learning sirName="Aamir Pinger" />
+      <Learning sirName="Mohsin Khalid" />
+      <Learning sirName="Basheer Aziz" />
+      <Learning sirName="Ameen Alam" />
     </div>
   );
 }
---
+++ src/learning.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Learning(props) {
+  return (
+    <div>
+      <h1>I am learning React from Bootcamp from Sir {props.sirName}</h1>
+    </div>
+  );
+}
+
+export default Learning;
